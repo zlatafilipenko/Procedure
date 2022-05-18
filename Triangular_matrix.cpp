d9df5b392d17e8d0274ee9db2af505eb92533983
zlatafilipenko <zlatoc17@gmail.com>
2022-05-19 00:43:44 +0700

    Lab3_Last_Fix

--- Triangular_matrix.cpp
+++ Triangular_matrix.cpp
@@ -20,11 +20,18 @@ void In_Triangular_matrix(int N, Triangular_matrix& T_m, ifstream& ifst)
     }
 }
 
-void Out_Triangular_matrix(int N, Triangular_matrix& T_m, ofstream& ofst)
+void Out_Triangular_matrix(Key_out K_o, int N, Triangular_matrix& T_m, ofstream& ofst)
 {
     ofst << "It's triangular matrix with dimension = " << N << endl;
-    
-    int Array_index = 0; //Иднекс для прохода ненулевых элементов матрицы
+
+    int Array_index = 0;
+
+    int** Temp_array = new int* [N];
+
+    for (int i = 0; i < N; i++)
+    {
+        Temp_array[i] = new int[N];
+    }
 
     for (int i = 0; i < N; i++)
     {
@@ -32,14 +39,66 @@ void Out_Triangular_matrix(int N, Triangular_matrix& T_m, ofstream& ofst)
         {
             if (i >= j)
             {
-                ofst << T_m.Array[Array_index] << " ";
+                Temp_array[i][j] = T_m.Array[Array_index];
                 Array_index++;
             }
             else
             {
-                ofst << "0 ";
+                Temp_array[i][j] = 0;
+            }
+        }
+    }
+
+    if (K_o == BY_LINE)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                ofst << Temp_array[i][j] << " ";
+            }
+            ofst << endl;
+        }
+    }
+    else if (K_o == BY_COLUMN)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                ofst << Temp_array[j][i] << " ";
+            }
+            ofst << endl;
+        }
+    }
+    else if (K_o == ONE_DIMENSIONAL)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                ofst << Temp_array[i][j] << " ";
             }
         }
         ofst << endl;
     }
+}
+
+int Sum_Triangular_matrix(int N, Triangular_matrix& T_m)
+{
+    int Temp_N = N;
+    int Array_size = 0;
+
+    while (Temp_N) {
+        Array_size += Temp_N;
+        Temp_N--;
+    }
+
+    int Sum = 0;
+
+    for (int i = 0; i < Array_size; i++) {
+        Sum += T_m.Array[i];
+    }
+
+    return Sum;
 }
--- Triangular_matrix.h
+++ Triangular_matrix.h
@@ -13,6 +13,9 @@ struct Triangular_matrix {
 void In_Triangular_matrix(int N, Triangular_matrix& T_m, ifstream& ifst);
 
 //Функция вывода треугольной матрицы
-void Out_Triangular_matrix(int N, Triangular_matrix& T_m, ofstream& ofst);
+void Out_Triangular_matrix(Key_out K_o, int N, Triangular_matrix& T_m, ofstream& ofst);
+
+//Функция подсчета суммы элементов треугольной матрицы
+int Sum_Triangular_matrix(int N, Triangular_matrix& T_m);
 
 #endif // TRIANGULAR_MATRIX_H
